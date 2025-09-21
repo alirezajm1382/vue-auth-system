d4a7bd4ca37d11a4826aa171879e3fab0762dc45
Alireza Javadzadeh M. <alirezajm@hotmail.com>
2025-09-21 18:17:29 +0330

    feat: theme setup + color palette

---
+++ src/components/shared/CustomInput.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <input
+    class="w-full h-[65px] text-base px-[40px] rounded-lg input input-bordered bg-neutral border-none"
+  />
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,29 @@ import daisyui from 'daisyui'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
   plugins: [daisyui],
+  theme: {
+    colors: {
+      'content-primary': '#fff',
+      'content-secondary': '#a6a7ad',
+      'card-background': '#ffe3ca',
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#FC728B',
+          secondary: '#F4C395',
+          accent: '#00bcda',
+          neutral: '#33394F',
+          'base-100': '#202433',
+          info: '#0074de',
+          success: '#00e437',
+          warning: '#ff9300',
+          error: '#ff728d',
+        },
+      },
+    ],
+  },
 }
